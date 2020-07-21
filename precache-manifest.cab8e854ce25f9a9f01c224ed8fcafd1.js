5a2cbb94ecbf28931717e7320f7b13c53204b940
Vishnu <pavi@pavis-MacBook-Pro.local>
2020-07-21 22:52:53 +0530

    Updates

---
+++ precache-manifest.cab8e854ce25f9a9f01c224ed8fcafd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ebc3e4fdc8e3b24b06d217ac22e057",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "7e73b44114fe14d338db",
+    "url": "/react-page/static/css/main.55f07782.chunk.css"
+  },
+  {
+    "revision": "4b1d9fdc0653ebf579d8",
+    "url": "/react-page/static/js/2.e1463447.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.e1463447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e73b44114fe14d338db",
+    "url": "/react-page/static/js/main.a3f5b53e.chunk.js"
+  },
+  {
+    "revision": "eb18cec803fae7134401",
+    "url": "/react-page/static/js/runtime-main.f8eddd1f.js"
+  }
+]);

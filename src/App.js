150c1b1a327393eac1f3bc4160b4c1b2dbc6a633
Vishnu <pavi@pavis-MacBook-Pro.local>
2020-07-21 22:37:17 +0530

    App commits

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Gh-Pages Tutorial</h1>
+      <p>This is a tutorial to deploy React app as github pages</p>
+      <p><strong>Developer Contact: </strong>Vishnu Prasad</p>
+    </div>
+  );
+}
+
+export default App;
